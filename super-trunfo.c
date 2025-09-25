0e7dedd86db6da048ae19e7ab8f89981610e7985
mathmadeiros <math.madeiros@outlook.com>
2025-09-24 22:29:12 -0300

    Update super-trunfo.c
    Aplicadas melhorias na organização do código, além de ajustes finos na visualização dos resultados

--- super-trunfo.c
+++ super-trunfo.c
@@ -6,7 +6,7 @@ int main() {
 
     // Modelo da carta 
     char estado[15];
-    char codigo[10];
+    int codigo; // Código IBGE com 7 dígitos
     char nome[50];
     int populacao;
     float pib; // em bilhões de reais
@@ -19,13 +19,13 @@ int main() {
     // Entrada de dados
     
     printf("Digite o nome da cidade: ");
-    scanf(" %[^\n]", nome);
+    scanf(" %[^\n]", nome); // Lê string com espaços
 
     printf("Digite o estado: ");
     scanf("%s", estado);
 
-    printf("Digite o código IBGE da cidade: ");
-    scanf("%s", &codigo);
+    printf("Digite o código IBGE da cidade(7 Dígitos): ");
+    scanf("%d", &codigo);
 
     printf("Digite a população: ");
     scanf("%d", &populacao);
@@ -43,24 +43,28 @@ int main() {
 
     // Calcula propriedades
     
-    pibConvertido = (double)pib * 1000000000;
-    pibPerCapita = pibConvertido / populacao;
-    densidadePopulacional = (float)populacao / area;
+    pibConvertido = (double)pib * 1000000000; // Convertendo PIB para reais
+    pibPerCapita = pibConvertido / populacao; // PIB per capita
+    densidadePopulacional = (float)populacao / area;  // Densidade populacional
 
     // Exibir carta
     printf("\n╔══════════════════════════════════════════════╗\n");
     printf("║         🏙️  SUPER TRUNFO - CIDADES            ║\n");
     printf("╚══════════════════════════════════════════════╝\n");
 
-    printf("📌  %s - %s\n", nome, estado);
+    printf("📌  Nome: %s - %s\n", nome, estado);
     printf("🆔  Código IBGE: %d\n", codigo);
-    printf("🏷️  Nome: %s\n", nome);
     printf("👥  População: %d habitantes\n", populacao);
-    printf("🌍  Área: %.2f km²\n", area);
+    printf("🌍  Área: %.3f km²\n", area); 
+    // O formato %.3f no printf exibe o número com 3 casas decimais após a vírgula
     printf("🏞️  Pontos Turísticos: %d\n", pontosTuristicos);
-    printf("📏  Densidade Populacional: %.2f hab/km²\n", densidadePopulacional);
+    printf("📏  Densidade Populacional: %.1f hab/km²\n", densidadePopulacional); 
+    // O formato %.1f no printf exibe o número com 1 casa decimail após a vírgula.
     printf("💰  PIB: R$ %.2f (%.0f bilhões aproximadamente)\n", pibConvertido, pib);
+    /* O formato %.2f no printf exibe o número com 2 casas decimais após a vírgula.
+    Já o %.0f, mostra apenas o valor inteiro, sem decimais.*/
     printf("📊  PIB per capita: R$ %.2f\n", pibPerCapita);
+    // O formato %.2f no printf exibe o número com 2 casas decimais após a vírgula.
     printf("════════════════════════════════════════════════\n");
 
 
